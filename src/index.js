8ad03f5ea2c427d386eaf334a2bff7c480c26c81
Susanta Biswas <susantasky@gmail.com>
2018-11-20 05:01:15 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ControlledInput from './components/ControlledInput';
 
 ReactDOM.render(
-  <div>Mount Components Here</div>,
+  <ControlledInput />,
   document.getElementById('root')
 );
