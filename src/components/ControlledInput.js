a186707f384d3e700b238ba097293c7af2ccda61
Adam N <adam.nawroc@gmail.com>
2019-01-03 18:11:19 +0000

    Done.

--- src/components/ControlledInput.js
+++ src/components/ControlledInput.js
@@ -1,1 +1,18 @@
-// Code ControlledInput Component Here
+// Code DigitalClicker Component Here
+import React from 'react';
+
+export default class ControlledInput extends React.Component {
+  state = {
+    firstName: "John",
+    lastName: "Henry"
+  }
+
+  render() {
+    return (
+      <form>
+        <input type="text" name="firstName" value={this.state.firstName} />
+        <input type="text" name="lastName" value={this.state.lastName} />
+      </form>
+    )
+  }
+}
